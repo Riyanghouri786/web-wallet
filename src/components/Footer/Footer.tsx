9e1acab1f86681bba01bf208c32bf857e4da9d73
Riyanghouri786 <riyanghouri66@gmail.com>
2025-04-05 09:20:23 -0700

    Name Set-Up

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,11 +7,11 @@ const Footer: FC = () => {
   return (
     <Box as="footer" p="1rem" position="sticky" bottom={0} zIndex={10} textAlign="center">
       <Link
-        href="https://github.com/Pedrojok01/Next-Web3-Boilerplate"
+        href="https://github.com/Riyanghouri786/web-wallet"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Don&apos;t forget to leave a ⭐️ on this boilerplate if you like it!
+        Don&apos;t forget to leave a ⭐️ on this Riyanghouri786 if you like it!
       </Link>
     </Box>
   );
